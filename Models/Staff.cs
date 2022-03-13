dc5c9f0cf9b10a74405ddcbcb000b4dbf9b40389
wp1admin <yang_changjun@simtech.a-star.edu.sg>
2022-03-13 17:42:10 +0800

    Add project files.

---
+++ Models/Staff.cs
@@ -0,0 +1,9 @@
+﻿namespace SMS.Models
+{
+    public class Staff
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+    }
+}
---
+++ Models/StaffContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace SMS.Models
+{
+    public class StaffContext: DbContext
+    {
+        public StaffContext(DbContextOptions<StaffContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Staff> TodoItems { get; set; } = null!;
+    }
+}
